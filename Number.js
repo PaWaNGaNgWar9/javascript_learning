975a92c62188740c61994a36d1c45a88d4607aef
PaWaNGaNgWar9 <pawangangwar073@gmail.com>
2025-12-03 11:17:36 +0530

    push Three files: Number.js, Test.js, Variable.js

---
+++ Number.js
@@ -0,0 +1,5 @@
+let score ="34abc";
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
---
+++ Variable.js
@@ -0,0 +1,5 @@
+const accountID = 12344;
+let userName = "JohnDoe";
+var isActive = true;
+accountID = 54321; // This will cause an error
+console.log(accountID);
