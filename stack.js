36ae773acf49fa60276b9429c1e17d0f8800eefa
PaWaNGaNgWar9 <pawangangwar073@gmail.com>
2025-12-03 11:47:50 +0530

    add heap features

--- stack.js
+++ stack.js
@@ -1,6 +1,16 @@
+// stack.js
 let Myname ="Virat Gangwar";
 let anotherName = Myname;
 console.log(anotherName);
 anotherName ="Pawan gangwar";
 console.log(Myname);
 console.log(anotherName);
+// heap.js
+let person1={
+    name:"Virat Gangwar",
+    age:24
+}
+let person2 = person1;
+person2.age=25;
+console.log(person1.age);
+console.log(person2.age);
