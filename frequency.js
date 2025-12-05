e8819746a0fde557a600ed9f11170f6388ab3925
PaWaNGaNgWar9 <pawangangwar073@gmail.com>
2025-12-05 11:29:26 +0530

    add frequency array

---
+++ frequency.js
@@ -0,0 +1,17 @@
+const arr = [1, 2, 2, 3, 4, 4, 4, 5];
+const visited = new Array(arr.length).fill(false);
+for(let i=0 ; i<arr.length; i++)
+{
+    if(visited[i]===true)
+        continue;
+    let count =0;
+    for(let j=0; j<arr.length; j++)
+    { 
+        if(arr[i]===arr[j])
+        {
+            count++;
+            visited[j]= true;
+        }
+    }
+     console.log("frequency"+count +"="+arr[i]);
+}
